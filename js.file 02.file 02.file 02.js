3fac3aed16ee2f464143f606ce6b8673d5d268c3
saireddytejasree <127979053+sairedddytejasree@users.noreply.github.com>
2023-03-30 22:54:04 +0530

    Add files via upload

---
+++ js.file 02.file 02.file 02.js
@@ -0,0 +1,20 @@
+let numbercontainer = document.getelement byid("num");
+let value="0";
+
+let h2 class = document.querySelector(".inc");
+let h2 class =document.querySelector(".dec");
+let h2 class =document.querySelector(".reset");
+
+button increase.addEventlistener("click", () =>{
+    value+++;
+    numbercontainer.textcontent= value;
+});
+btndec.addEventListener("Click",() =>{
+    value---;
+    numbercontainer.textcontent=value;
+});
+{
+display.textcontent= value;
+}
+
+
