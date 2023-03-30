b24deb0a79997868df28dafaf8afc78bf85ea51c
saireddytejasree <127979053+sairedddytejasree@users.noreply.github.com>
2023-03-30 23:34:19 +0530

    Add files via upload

---
+++ jss.file 03.js
@@ -0,0 +1,51 @@
+//Function for addition values
+function add() {
+    var one =
+document.getElementById('no1').Value
+var two=
+document.getElementById('no2').Value
+var numberone = perseInt(one);
+var numbertwo = perseInt(two);
+var result = numberone + numbertwo;
+ alert('Result is: ' +
+result);
+}
+//function for subtracting values
+function subtracting() {
+    var one =
+document.getElementById('no1').Value
+  var two =
+  document.getElementById('no2').Value
+  var numberone = parseInt(one);
+  var numbertwo = parseInt(two);
+  var result = numberone - numbertwo;
+  alert('result is: ' - 
+  result);
+}
+//Function for Dividing values
+function Dividing() {
+    var one =
+document.getElementById('no1').Value
+var two=
+document.getElementById('no2').Value
+var numberone = perseInt(one);
+var numbertwo = perseInt(two);
+var result = numberone / numbertwo;
+ alert('Result is: ' /
+result);
+}
+//Function for multiplication values
+function add() {
+    var one =
+document.getElementById('no1').Value
+var two=
+document.getElementById('no2').Value
+var numberone = perseInt(one);
+var numbertwo = perseInt(two);
+var result = numberone * numbertwo;
+ alert('Result is: ' *
+result);
+}
+            
+        
+
